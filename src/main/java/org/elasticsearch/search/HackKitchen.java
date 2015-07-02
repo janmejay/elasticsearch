737799168d7d3eb42c4a1c31e193825b019cda40
Janmejay Singh <singh.janmejay@gmail.com>
2015-07-02 20:48:20 +0530

    setup an easy way to overwrite fields

--- src/main/java/org/elasticsearch/search/HackKitchen.java
+++ src/main/java/org/elasticsearch/search/HackKitchen.java
@@ -19,11 +19,8 @@
 
 package org.elasticsearch.search;
 
-import org.apache.lucene.queries.FilterClause;
-import org.apache.lucene.search.Filter;
-import org.apache.lucene.search.FilteredQuery;
 import org.apache.lucene.search.NumericRangeFilter;
-import org.apache.lucene.search.Query;
+import org.apache.lucene.search.NumericRangeQuery;
 import org.elasticsearch.common.logging.ESLogger;
 import org.elasticsearch.common.logging.Loggers;
 import org.elasticsearch.common.lucene.search.XBooleanFilter;
@@ -38,20 +35,53 @@ import java.util.*;
  */
 public class HackKitchen {
     private static final ESLogger logger = Loggers.getLogger("hackery");
+    private static final String OBJECT_CLASS_NAME = Object.class.getCanonicalName();
 
     public void validateQueryAndFilter(SearchContext context) {
         try {
+            //Sanitization 1: allow only upto 6 hours of viewing window
             List<XBooleanFilter> instances = findType(context, XBooleanFilter.class);
-            for (XBooleanFilter instance : instances) {
-                logger.debug("------------------------------");
-                logger.debug("XBooleanFilter: {}", instance);
-                List<NumericRangeFilter> nrfs = findType(instances, NumericRangeFilter.class);
+            List<NumericRangeQuery> nrqs = findType(instances, NumericRangeQuery.class);
+            for (NumericRangeQuery nrq : nrqs) {
+                String field = nrq.getField();
+                if ("@timestamp".equals(field)) {
+                    Long min = (Long) nrq.getMin();
+                    Long max = (Long) nrq.getMax();
+                    long diffMs = max - min;
+                    int maxAllowedWidth = 6 * 60 * 60 * 1000;
+                    if (diffMs > maxAllowedWidth) {
+                        min = max - maxAllowedWidth;
+                        logger.warn("Reducing @timestamp filter from {} hrs to {} hrs.", ((double) diffMs) / 1000 / 60 / 60, (max - min) / 1000 / 60 / 60);
+                        setValue(nrq, "min", min);
+                    }
+                }
             }
         } catch (Exception e) {
         }
     }
 
+    private void setValue(Object o, String name, Object value) {
+        Class<?> type = o.getClass();
+        do {
+            try {
+                Field field = type.getField(name);
+                field.setAccessible(true);
+                field.set(o, value);
+                return;
+            } catch (NoSuchFieldException e) {
+                type = type.getSuperclass();
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        } while (OBJECT_CLASS_NAME.equals(type.getCanonicalName()));
+    }
+
     private <T> void checkTypeAndAdd(Object o, Class<T> targetType, List<T> collector, int level) {
+//        StringBuilder b = new StringBuilder();
+//        for (int i = 0; i < level; i++) {
+//            b.append(">> ");
+//        }
+//        logger.debug("{}{}", b.toString(), o.getClass());
         if (targetType.isAssignableFrom(o.getClass())) {
             collector.add((T) o);
         }
@@ -66,55 +96,59 @@ public class HackKitchen {
 
     private <T> void findTypeRec(Object o, Map<Class, Set<Object>> seen, Class<T> targetType, List<T> collector, int level) {
         if (o == null) return;
-        Set<Object> seenSet = seen.get(o.getClass());
+        Class<?> oType = o.getClass();
+        Set<Object> seenSet = seen.get(oType);
         if (seenSet == null) {
             seenSet = new HashSet<>();
-            seen.put(o.getClass(), seenSet);
+            seen.put(oType, seenSet);
         }
         if (seenSet.contains(o)) return;
         seenSet.add(o);
         checkTypeAndAdd(o, targetType, collector, level);
-        Field[] fields = o.getClass().getDeclaredFields();
-        for (Field field : fields) {
-            int mod = field.getModifiers();
-            if (field.isSynthetic() || field.isEnumConstant() || (mod & Modifier.STATIC) != 0) {
-                continue;
+        if (List.class.isAssignableFrom(oType)) {
+            for (Object o1 : (List) o) {
+                findTypeRec(o1, seen, targetType, collector, level + 1);
             }
-            Class<?> t = field.getType();
-            if (t == boolean.class
-                    || t == byte.class
-                    || t == char.class
-                    || t == short.class
-                    || t == int.class
-                    || t == long.class
-                    || t == float.class
-                    || t == double.class
-                    || t == void.class) {
-                continue;
+        } else if (Map.class.isAssignableFrom(oType)) {
+            Map o1 = (Map<Object, Object>) o;
+            for (Object o2k : o1.keySet()) {
+                Object o2v = o1.get(o2k);
+                findTypeRec(o2k, seen, targetType, collector, level + 1);
+                findTypeRec(o2v, seen, targetType, collector, level + 1);
             }
-            if (t.getCanonicalName().startsWith("java.lang.")) continue;
+        } else {
+            do {
+                Field[] fields = oType.getDeclaredFields();
+                for (Field field : fields) {
+                    int mod = field.getModifiers();
+                    if (field.isSynthetic() || field.isEnumConstant() || (mod & Modifier.STATIC) != 0) {
+                        continue;
+                    }
+                    Class<?> t = field.getType();
+                    if (t == boolean.class
+                            || t == byte.class
+                            || t == char.class
+                            || t == short.class
+                            || t == int.class
+                            || t == long.class
+                            || t == float.class
+                            || t == double.class
+                            || t == void.class) {
+                        continue;
+                    }
+                    if (t.getCanonicalName().startsWith("java.lang.")) continue;
 
-            field.setAccessible(true);
-            Object o1 = null;
-            try {
-                o1 = field.get(o);
-                if (List.class.isAssignableFrom(t)) {
-                    for (Object o2 : (List) o1) {
-                        findTypeRec(o2, seen, targetType, collector, level + 1);
+                    field.setAccessible(true);
+                    Object o1 = null;
+                    try {
+                        o1 = field.get(o);
+                        findTypeRec(o1, seen, targetType, collector, level + 1);
+                    } catch (IllegalAccessException e) {
+                    } catch (NullPointerException e) {
                     }
-                } else if (Map.class.isAssignableFrom(t)) {
-                    Map o2 = (Map<Object, Object>) o1;
-                    for (Object o3k : o2.keySet()) {
-                        Object o3 = o2.get(o3k);
-                        findTypeRec(o3k, seen, targetType, collector, level + 1);
-                        findTypeRec(o3, seen, targetType, collector, level + 1);
-                    }
-                } else {
-                    findTypeRec(o1, seen, targetType, collector, level + 1);
                 }
-            } catch (IllegalAccessException e) {
-            } catch (NullPointerException e) {
-            }
+                oType = oType.getSuperclass();
+            } while (!oType.getCanonicalName().startsWith("java.lang."));
         }
     }
 }
