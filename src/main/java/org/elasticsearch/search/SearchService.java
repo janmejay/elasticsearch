f51e421d70e1d4d2fb55541bddef0af672ad3444
Janmejay Singh <singh.janmejay@gmail.com>
2015-10-14 20:00:22 +0530

    Revert "preparing for convinient hotswaps"
    This reverts commit d7a60c94469a24c9ce8089683bde788117c68039.

--- src/main/java/org/elasticsearch/search/SearchService.java
+++ src/main/java/org/elasticsearch/search/SearchService.java
@@ -557,16 +557,11 @@ public class SearchService extends AbstractLifecycleComponent<SearchService> {
     }
 
     private void validateQueryAndFilter(SearchContext context) {
-        try {
-            Set<Object> set = new HashSet<>();
-            walkAndCall(context, new FilterFinder(), set);
-        } catch (Exception e) {
-
-        }
+        Set<Object> set = new HashSet<>();
+        walkAndCall(context, new FilterFinder(), set);
     }
 
     private void walkAndCall(Object o, FilterFinder listener, Set<Object> set) {
-        //if (true) return;
         if (set.contains(o)) return;
         if (o == null) return;
         set.add(o);
