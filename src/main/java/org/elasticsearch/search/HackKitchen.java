8757e1eeb2abf1b2469f108dd42a2bebdefbbbe3
Janmejay Singh <singh.janmejay@gmail.com>
2015-10-14 19:59:44 +0530

    Revert "One of the classes BufferBytesReference assumes whoever compares to it is the same type, java basics anyone?"
    This reverts commit c7eb2a1d9e3b09abded4eca362a053f2c3de1b4e.

--- src/main/java/org/elasticsearch/search/HackKitchen.java
+++ src/main/java/org/elasticsearch/search/HackKitchen.java
@@ -25,10 +25,7 @@ import org.elasticsearch.common.logging.Loggers;
 import org.elasticsearch.search.internal.SearchContext;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 /**
@@ -39,10 +36,10 @@ public class HackKitchen {
 
     public void validateQueryAndFilter(SearchContext context) {
         try {
-            Map<Class, Set<Object>> map = new HashMap<>();
-            walkAndCall(context, map);
+            Set<Object> set = new HashSet<>();
+            walkAndCall(context, set);
         } catch (Exception e) {
-            System.err.println("Exception occured: " + e);
+
         }
     }
 
@@ -52,41 +49,22 @@ public class HackKitchen {
         }
     }
 
-    private void walkAndCall(Object o, Map<Class, Set<Object>> seen) {
-        //if (true) return;
-        Set<Object> seenSet = seen.get(o.getClass());
-        if (seenSet == null) {
-            seenSet = new HashSet<>();
-            seen.put(o.getClass(), seenSet);
-        }
-        if (seenSet.contains(o)) return;
+    private void walkAndCall(Object o, Set<Object> set) {
+        if (true) return;
+        if (set.contains(o)) return;
         if (o == null) return;
-        seenSet.add(o);
+        set.add(o);
         doValidate(o);
         Field[] fields = o.getClass().getDeclaredFields();
         for (Field field : fields) {
-            int mod = field.getModifiers();
-            if (field.isSynthetic() || field.isEnumConstant() || (mod & Modifier.STATIC) != 0) {
-                continue;
-            }
-            if (field.getType() == boolean.class
-                    || field.getType() == byte.class
-                    || field.getType() == char.class
-                    || field.getType() == short.class
-                    || field.getType() == int.class
-                    || field.getType() == long.class
-                    || field.getType() == float.class
-                    || field.getType() == double.class
-                    || field.getType() == void.class) {
-                continue;
-            }
             field.setAccessible(true);
-            Object o1 = null;
             try {
-                o1 = field.get(o);
-                walkAndCall(o1, seen);
+                Object o1 = field.get(o);
+                walkAndCall(o1, set);
             } catch (IllegalAccessException e) {
             }
         }
     }
+
+
 }
